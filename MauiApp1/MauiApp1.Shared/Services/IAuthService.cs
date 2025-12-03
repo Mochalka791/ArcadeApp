6c64aca3c732c87c40e102a90d1f058439bc9c22
Viktor Rozlomii <viktor.rozlomii@aturis.com>
2025-12-03 09:59:08 +0100

    Add project files.

---
+++ MauiApp1/MauiApp1.Shared/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiApp1.Shared.Services;
+
+public interface IAuthService
+{
+    Task<bool> RegisterAsync(string email, string password);
+    Task<User?> LoginAsync(string email, string password);
+}
---
+++ MauiApp1/MauiApp1.Shared/Services/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiApp1.Shared.Services;
+
+public sealed class User
+{
+    public string Email { get; set; } = string.Empty;
+    // später -> verschlüsseln
+    public string Password { get; set; } = string.Empty;
+}
